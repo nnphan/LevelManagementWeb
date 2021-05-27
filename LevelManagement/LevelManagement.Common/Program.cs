d51daf945867c496ba356bd6196445da7d274a82
nnphan <nnphan20041997@gmail.com>
2021-05-27 10:03:13 +0700

    codebase init

---
+++ LevelManagement/LevelManagement.Common/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LevelManagement.Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LevelManagement/LevelManagement.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LevelManagement.Models
+{
+    public class Class1
+    {
+    }
+}
